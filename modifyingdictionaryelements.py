bab8c34fb977a856c39c20e6be531172ab8a911b
Riya-3421 <g.riya160106@gmail.com>
2026-06-11 22:57:59 +0530

    modifyingdictionaryelements

---
+++ modifyingdictionaryelements.py
@@ -0,0 +1,14 @@
+student={"name":"Riya","age":22,"grade":'A'}
+print(student)
+##Modifying Dictionary elements
+student["age"]=22
+print(student)
+student["address"]="India"
+print(student)
+del student["grade"]
+print(student)
+
+
+
+
+
