975a86840d20b7928c96a768a04f8627f0fd3632
Riya-3421 <g.riya160106@gmail.com>
2026-06-12 20:45:42 +0530

    Dictionarymethods

---
+++ Dictionarymethods.py
@@ -0,0 +1,9 @@
+student={"name":"Riya","age":22,"grade":'A'}
+print(student)
+##Dictionary methods
+keys=student.keys()
+print(keys)
+values=student.values()
+print(values)
+items=student.items()
+print(items)
