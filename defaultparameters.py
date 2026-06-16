7a251ba4c124880cb8a36854085f9c97cf2f323f
Riya-3421 <g.riya160106@gmail.com>
2026-06-16 21:45:56 +0530

    defaultparameters

---
+++ defaultparameters.py
@@ -0,0 +1,8 @@
+##Default parameters
+def greet(name):
+    print(f"Hello{name}Welcome to the paradise")
+greet("Riya")
+
+def greet(name="guest"):
+    print(f"Hello{name}Welcome to the paradise")
+greet()
