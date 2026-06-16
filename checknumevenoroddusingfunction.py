985f5b12fee523db0e3584854e794b7e6065cffb
Riya-3421 <g.riya160106@gmail.com>
2026-06-16 21:31:19 +0530

    checknumevenoddusingpython

---
+++ checknumevenoroddusingfunction.py
@@ -0,0 +1,14 @@
+##To check the num 29 is even or odd
+num=29
+if num % 2 == 0:
+    print("the number is even")
+else:
+    print("the number is odd")
+
+##using function
+def even_or_odd(num):
+    """This function finds even or odd"""
+    if num % 2 == 0:
+        print("the number is even")
+    else:
+        print("the number is odd")
