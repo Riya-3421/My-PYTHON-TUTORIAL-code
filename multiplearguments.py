a8a3c2d92487d5275bddbb08484665839e8e58c2
Riya-3421 <g.riya160106@gmail.com>
2026-06-24 21:58:05 +0530

    multiplearguments

---
+++ multiplearguments.py
@@ -0,0 +1,8 @@
+##Define a function that takes two integers as input and returns their sum.
+##Test the function with different inputs.
+def add(x,y):
+    return x+y
+
+#Test
+print(add(5,8))
+print(add(16,12))
