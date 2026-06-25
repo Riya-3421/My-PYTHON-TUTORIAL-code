2ca5aaa30cc619b7354823264e3b654d6251e6d0
Riya-3421 <g.riya160106@gmail.com>
2026-06-25 14:21:44 +0530

    returningmultiplevalues

---
+++ returningmultiplevalues.py
@@ -0,0 +1,8 @@
+##Define a function that takes a single integer as input and returns the integer squared,cubed and raised the power of four.
+#Test the function with different inputs.
+def powers(x):
+    return x**2,x**3,x**4
+
+#test
+print(powers(2))
+print(powers(3))
